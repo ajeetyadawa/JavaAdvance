52a9785c0746efc87c2a33865b7ab6ac19d630a4
Ajeet Yadav <38289540+ajitjss@users.noreply.github.com>
2019-10-08 12:40:23 +0530

    Adding first commit

---
+++ qaedupointjava/src/main/java/com/advacejavaforqa/qaedupointjava/App.java
@@ -0,0 +1,13 @@
+package com.advacejavaforqa.qaedupointjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
