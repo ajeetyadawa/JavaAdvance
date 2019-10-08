cbb340d2790d0e1390fd474bcb1e7de9c73b35ae
Ajeet Yadav <38289540+ajitjss@users.noreply.github.com>
2019-10-08 12:56:09 +0530

    Second commit

--- qaedupointjava/src/main/java/base/test.java
+++ qaedupointjava/src/main/java/base/test.java
@@ -3,5 +3,6 @@ package base;
 public class test {
 	public static void main(String[] args) {
 	System.out.println("Program to test Java");
+	System.out.println("Program to test second commit Java");
 }
 }
